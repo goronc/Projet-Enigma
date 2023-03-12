67b156439061a22190269b1d11a4185afb10347c
goronc <108907603+goronc@users.noreply.github.com>
2023-03-12 20:13:44 +1100

    Update Cesar.py

--- Cesar.py
+++ Cesar.py
@@ -1,38 +1,68 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-# alphabt_Maj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def chiffrer(mot, decalage):
+def chiffrer(chaine,decalage):  
     res = ''
-    for i in range(len(mot)):   # Parcours lettre par lettre du mot a chiffrer
-        if mot[i] == ' ':   # Si il y a un espace mettre un espace  
+    for i in range(len(chaine)):    # parcour de la chaine a chiffrer
+        j = 0
+        if chaine[i] == ' ':    #exception si le caractere est un espace on laisse l'espace
             res += ' '
         else:
-            for j in range(len(alphabet)):   # Parcours de l'alphabet lettre par lettre
-                if alphabet[j] == mot[i]:   # Si la lettre du mot est = à la lettre de l'alphabet, on continue
-                    total = j + decalage  # total = la somme de la position de la lettre du mot dans l'alphabet et du décalage
-                    while total > 25:   # Tant que le total > 25, on lui soustrait 26
-                        total = total - 26
-                    res += alphabet[total]   # On ajoute la lettre chiffrer
+            while(alphabet[j] != chaine[i]):    # recherche du caractere a chiffrer dans l'alphabet de référence
+                j += 1
+            if j + decalage > len(alphabet):    # cas si le caractere + le decalage > 26 
+                j = len(alphabet) - j       # si oui on retourne au debut
+                res += alphabet[j]
+            else:                           # si non on ajoute juste a res le caractere + le decalage
+                res += alphabet[j+decalage]
     return res
 
-
-def dechiffrer(mot, decalage):
+def dechiffrer(chaine,decalage):
     res = ''
-    for i in range(len(mot)):   # Parcours lettre par lettre du mot a chiffrer
-        if mot[i] == ' ':   # Si il y a un espace mettre un espace  
+    for i in range(len(chaine)):    # parcour de la chaine a chiffrer
+        j = 0
+        if chaine[i] == ' ':     #exception si le caractere est un espace on laisse l'espace
             res += ' '
         else:
-            for j in range(len(alphabet)):   # Parcours de l'alphabet lettre par lettre
-                if alphabet[j] == mot[i]:   # Si la lettre du mot est = à la lettre de l'alphabet, on continue
-                    total = j - decalage   # total = la soustraction de la position de la lettre du mot dans l'alphabet et du décalage
-                    while total < 0:   # Tant que le total < 0, on lui ajoute 26
-                        total = total + 26
-                    res += alphabet[total]   # On ajoute la lettre dechiffrer
+            while(alphabet[j] != chaine[i]):         # recherche du caractere a chiffrer dans l'alphabet de référence
+                j += 1
+            if j - decalage < 0:            # cas si le caractere - le decalage < 0
+                j = len(alphabet) - abs(j)  # si oui on continue par la fin (valeurs absolus car j < 0 dans ce cas la)
+                res += alphabet[j]
+            else:                           # si non on ajoute juste a res le caractere - le decalage
+                res += alphabet[j-decalage] 
     return res
 
+def frequence_lettre(dico,chaine):
+    for i in range(len(chaine)):
+        if chaine[i] in dico.keys():
+            dico[chaine[i]] += 1 
+        else:
+            dico[chaine[i]] = 1 
+    return dico
 
-a = chiffrer("hello world", 3)
-print(a)
-b = dechiffrer("khoor zruog", 3)
-print(b)
+# Test ATTENTION carractere spéciaux non compatible
+# 2 possibilitées a choisir pour les caracteres spéciaux  soit les rajoutez a la fin de l'alphabet de reference (abc..xyz!?/:;.?,) soit les laissez tel quel comme les espaces
+x = "hello world"
+y = 3
+dico = {}
 
+z = chiffrer(x,y)
+print(z)
+w = dechiffrer(z,y)
+print(w)
+frequence_lettre(dico,z)
+
+
+fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+categories = [i for i in dico.keys()]
+ax.set_title("Fréquence des lettre dans la chaine de caractere")
+ax.set_ylabel("Nb répétition dans la chaine")
+ax.set_xlabel("Lettres")
+
+ax.bar(categories,  dico.values())
+plt.show()
+ 
